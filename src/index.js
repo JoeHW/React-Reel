09271d77f149e59974a26eb3a3e322b682740cba
Joe Williamson <jhwilliamson@outlook.com>
2017-09-13 16:34:35 -0400

    YouTube api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
-const API_KEY = 'AIzaSyC4_4ZqcQ4-esb3X5WZbD_I8gccMzOWW3Y';
+const API_KEY = 'AIzaSyCRA_dR7KI7n-mAmjYpGa3AvZ_goVmrCSY';
 
-const App = () => {
+
+YTSearch({key: API_KEY, term: 'surfboards'}, (data) => console.log(data)); 
+
+
+class App extends Component {
+ render () {
     return (
      <div>
       <SearchBar />
      </div>
- )
+  )}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
+
+
