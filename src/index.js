98d21101f89a855eb9d696d6b21acd3f20ad7876
Joe Williamson <jhwilliamson@outlook.com>
2017-09-13 13:17:13 -0400

    wireframing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-mport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyC4_4ZqcQ4-esb3X5WZbD_I8gccMzOWW3Y';
 
+const App = () => {
+    return (
+     <div>
+      <SearchBar />
+     </div>
+ )
+}
 
+ReactDOM.render(<App />, document.querySelector('.container'));
