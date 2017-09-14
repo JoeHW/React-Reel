04136cf150e66e4d229c5156d170f136ba43f793
Joe Williamson <jhwilliamson@outlook.com>
2017-09-14 13:31:54 -0400

    Media list

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyCRA_dR7KI7n-mAmjYpGa3AvZ_goVmrCSY';
 
-
-YTSearch({key: API_KEY, term: 'surfboards'}, (data) => console.log(data)); 
-
-
 class App extends Component {
+ constructor(props) {
+  super(props);
+  
+  this.state = { videos: [] };
+     
+  YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.setState({ videos })
+  });
+ }
+    
  render () {
     return (
      <div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />        
      </div>
-  )}
+  );
+ }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
